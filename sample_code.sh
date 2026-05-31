fae7d22081b2bb10155602993743287f0decdbb1
rakeshbathula421-maker <rakeshbathula421@gmail.com>
2026-05-31 23:03:09 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -2,7 +2,6 @@
 #
 
 echo "This is an Samples Script from GitHub"
-https://github.com/rakeshbathula421-maker/devops-helloworld.git
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
